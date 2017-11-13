ac7b34bf7c3753a24fc7407ce2f9afb55b39d715
capriciousalex <capriciousalex@users.noreply.github.com>
2017-11-13 19:54:21 +0000

    change from mysql to $_POST

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
 const Discord = require("discord.js");
-var mysql = require("mysql");
+var request = require("request");
 const bot = new Discord.Client();
-
 const token = 'MzM5MTExNTMzODIzMDAwNTc3.DFf7ww.cCYB6D7pExxX73QPE0FKkXgPeys';
-
-var con = mysql.createConnection({
-  host: "mysql.stackcp.com",
-  port: "49266",
-  user: "emojibot-primary-31366625",
-  password: "f53497dn8j",
-  database: "emojibot-primary-31366625"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
 
 bot.on('ready', () => {
   console.log('emoji-bot.js is ready!');
@@ -26,9 +12,16 @@ bot.on('message', message => {
   if (message.author.bot) return;
 
   var sql = "SELECT * FROM autoresponses WHERE cmd = '" + message.content + "'";
-  con.query(sql, function (err, result) {
-    try {message.channel.send(result[0].msg);} catch(err) {return;};
-  });
+
+  request.post(
+    'https://zeue.eu/api/emojibot.php',
+    { json: { msg: message.content } },
+    function (error, , body) {
+      if (!error && response.statusCode == 200) {
+        try {message.channel.send(body);} catch(error) {return;};
+      }
+    }
+  );
 });
 
 bot.login(token);
