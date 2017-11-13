6f2637e1f0bd1a650c005348382942bb8c89cf6d
Alex <capriciousalex@users.noreply.github.com>
2017-11-13 20:33:21 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-var request = require("request");
+const request = require("request");
 const bot = new Discord.Client();
 const token = 'MzM5MTExNTMzODIzMDAwNTc3.DFf7ww.cCYB6D7pExxX73QPE0FKkXgPeys';
 
@@ -11,17 +11,14 @@ bot.on('message', message => {
 
   if (message.author.bot) return;
 
-  var sql = "SELECT * FROM autoresponses WHERE cmd = '" + message.content + "'";
-
   request.post(
-    'https://zeue.eu/api/emojibot.php',
-    { json: { msg: message.content } },
-    function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        try {message.channel.send(body);} catch(error) {return;};
-      }
+    'https://zeue.eu/api/emojibot.php'+message.content,
+    function (error, response, body) 
+    {
+      if (!error && response.statusCode == 200) {try {message.channel.send(body);} catch(error) {return;};}
     }
   );
+
 });
 
 bot.login(token);
