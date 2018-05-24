c92057e8296bc023b2adaee571e65ab487351e26
Zeue <alex@peccy.net>
2018-05-24 12:59:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
-// CONFIGURATION
-const token = 'TOKEN_HERE';
-const owner = "101435120161927168";
-const adminArr = [
-    "165571041433878529"
-];
+const config = require("./config.json");
 
-// APPLICATION
 const Discord = require("discord.js");
+const fs = require('fs');
+
 const bot = new Discord.Client();
 
+// check if json file exists, create if not
+if (fs.existsSync("./store.json")) {
+    //...
+} else {
+    //...
+}
+
 bot.on('ready', () => {
-  console.log('EmojiBot is ready!');
+    console.log('EmojiBot is ready!');
 });
 
 bot.on('message', message => {
 
-  if (message.author.bot) return;
-
-  // prepare message for checks, if input is "!cmd arg1 arg2 arg3" it returns: 
-    // command["cmd"]
-    // args["arg1", "arg2", "arg3"]
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
+    if (message.author.bot || !message.content.startsWith(config.prefix)) return;
+    
+    // prepare message for checks, if input is "!cmd arg1 arg2 arg3" it returns: 
+        // command["cmd"]
+        // args["arg1", "arg2", "arg3"]
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
   
-  // test command
-  if (command == "") {
-      //...
-  }
+    switch (command) {
+      case "ping" :
+        message.channel.send('Pong!');
+        break;
+      case "asl" :
+        let [age, sex, location] = args;
+        message.reply(`Hello ${message.author.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);
+        break;
+    }
 
 });
 
-bot.login(token);
+bot.login(config.token);
